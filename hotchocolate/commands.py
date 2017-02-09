5def7bc5cba5bccacacab7a7f5de9f4ea8ef61ed
Emma Krantz <toxicteacakes@gmail.com>
2017-02-09 12:59:15 +0800

    Added command parser and a look command.

--- hotchocolate/commands.py
+++ hotchocolate/commands.py
@@ -1,17 +1,70 @@
-commandMap = {"look": look
-			"look at": look
-			"l": look}
+from hotchocolate.game import *
 
-def tokenize(command):
-	wordList = command.split()
+class CommandInterpreter():
+	"""
+	Parses user input and executes appropriate command, if any.
+	:param game: object representing currently running game
+	:type game: Game
+	:param commandDict: map from a command's text to the function that text
+		should trigger. Will use the commands implemented in BasicCommands by
+		default.
+	:type commandDict: dict(string, function)
+	"""
+	def __init__(self, game, commandDict=None):
+		self.game = game
+		if commandDict is None:
+			self.commandDict = basicCommandDict
+		else:
+			self.commandDict = commandDict
 
-	verbSoFar = ""
-	for word in wordList:
-		verbSoFar += word
-		if verbSoFar not in commandMap:
-			break
+	def parse(self, inp):
+		"""
+		Very simple parser for commands. Will look for a known command at the
+			beginning of the input, and execute the corresponding function (if
+			this exists) with the remaining input as arguments
+		:param inp: the input to parser
+		:type inp: string
+		"""
+		wordList = inp.split()
 
-	verb = verbSoFar
+		verbSoFar = ""
+		verbIdx = -1
+		for word in wordList:
+			if verbSoFar + word not in self.commandDict:
+				break
+			verbSoFar += word
+			verbIdx += 1
 
-def look(gameState, args):
-	pass
+		if verbIdx == -1:
+			# didn't recognise verb
+			self.game.ui.displayText("I couldn't understand that.")
+			return
+
+		verb = verbSoFar
+
+		if verbIdx == (len(wordList) -1):
+			# no words apart from verb in command, so no command has no
+			# arguments
+			self.commandDict[verb](self.game)
+		else:
+			args = wordList[verbIdx+1:]
+			self.commandDict[verb](self.game, args)
+
+class BasicCommands():
+	"""
+	Implementations of some basic, core commands
+	"""
+	@staticmethod
+	def look(game):
+		"""
+		Displays the room text
+		:param game: the game object to get
+		"""
+		print("run")
+		if not game.isRunning():
+			raise GameNotRunningException()
+		else:
+			game.displayCurrentRoom()
+
+basicCommandDict = {"look": BasicCommands.look,
+			"l": BasicCommands.look}
--- hotchocolate/game.py
+++ hotchocolate/game.py
@@ -1,13 +1,28 @@
 import hotchocolate.ui as ui
 import hotchocolate.worldobjects as worldobjects
+import hotchocolate.commands as commands
 import yaml
 
-class Game():
+class _Singleton(type):
+	_instances = {}
+	def __call__(cls, *args, **kwargs):
+		if cls not in cls._instances:
+			cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
+		return cls._instances[cls]
+
+class Game(metaclass=_Singleton):
+	running = False
 	def __init__(self, roomFilePath, uiClass=ui.CommandLineUI):
 		self.currentRoom = None
 		self.roomList = None
 		self.ui = uiClass()
 		self.roomFilePath = roomFilePath
+	@staticmethod
+	def isRunning():
+		return Game.running
+
+	def getInstance(self):
+		return self
 
 	def loadData(self):
 		f = open(self.roomFilePath)
@@ -16,10 +31,22 @@ class Game():
 
 	def mainLoop(self):
 		while True:
-			roomDesc = self.currentRoom.outputText()
-			self.ui.displayText(roomDesc)
 			inp = self.ui.getUserInput()
+			self.commandInterpreter.parse(inp)
+
+	def displayCurrentRoom(self):
+		roomDesc = self.currentRoom.outputText()
+		self.ui.displayText(roomDesc)
 
 	def run(self):
+		Game.running = True
+		self.commandInterpreter = commands.CommandInterpreter(self)
 		self.loadData()
+		self.displayCurrentRoom()
 		self.mainLoop()
+
+class GameNotRunningException(Exception):
+	def __init__(self, msg=None):
+		if msg is None:
+			msg = "A Game instance is not currently running."
+		super(GameNotRunningException, self).__init__(msg)
